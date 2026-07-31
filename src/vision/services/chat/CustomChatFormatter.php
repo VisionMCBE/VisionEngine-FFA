1b321f7decdd4d31c1ae10538695f51358d73221
NaySurGithub <NaySurGithub@users.noreply.github.com>
2026-07-31 13:49:23 +0200

    fix

--- src/vision/services/chat/CustomChatFormatter.php
+++ src/vision/services/chat/CustomChatFormatter.php
@@ -5,7 +5,7 @@ namespace vision\services\chat;
 use pocketmine\player\chat\ChatFormatter;
 use pocketmine\utils\TextFormat;
 
-class CustomChatFormatter extends ChatFormatter implements ChatFormatter {
+final class CustomChatFormatter implements ChatFormatter {
     public function __construct(private readonly string $prefix) {}
 
     public function format(string $username, string $message): string {
